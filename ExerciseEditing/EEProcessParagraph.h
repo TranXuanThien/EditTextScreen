189da875f7f3b829d118ec48fc1f9dd2b33fa60d
SarkozyTran <sarkozytran@SarkozyTrans-Mac.local>
2016-02-25 07:00:43 -0800

    [r] move all logic to EEProcessParagraph

---
+++ ExerciseEditing/EEProcessParagraph.h
@@ -0,0 +1,13 @@
+//
+//  EEProcessParagraph.h
+//  ExerciseEditing
+//
+//  Created by SarkozyTran on 2/25/16.
+//  Copyright © 2016 SarkozyTran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EEProcessParagraph : NSObject
+
+@end
---
+++ ExerciseEditing/EEProcessParagraph.m
@@ -0,0 +1,13 @@
+//
+//  EEProcessParagraph.m
+//  ExerciseEditing
+//
+//  Created by SarkozyTran on 2/25/16.
+//  Copyright © 2016 SarkozyTran. All rights reserved.
+//
+
+#import "EEProcessParagraph.h"
+
+@implementation EEProcessParagraph
+
+@end
